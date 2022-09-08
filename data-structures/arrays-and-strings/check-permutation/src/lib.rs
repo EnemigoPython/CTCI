2b5e249aa4445a509eeef551fe09aabcc87a456c
Basil Eagle <basileagle@gmail.com>
2022-09-08 18:12:56 +0100

    recommit

---
+++ data-structures/arrays-and-strings/check-permutation/src/lib.rs
@@ -0,0 +1,15 @@
+// prompt: Given two strings, write a method to decide if one is a permutation of the other.
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
