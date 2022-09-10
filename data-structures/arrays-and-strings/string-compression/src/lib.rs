8fbe994c3498548e652c767a0f0d9a6462418b36
Basil Eagle <basileagle@gmail.com>
2022-09-10 10:38:24 +0100

    string compression test cases created

--- data-structures/arrays-and-strings/string-compression/src/lib.rs
+++ data-structures/arrays-and-strings/string-compression/src/lib.rs
@@ -4,17 +4,32 @@
 // the original string, your method should return the original string. You can
 // assume the string has only uppercase and lowercase letters (a-z).
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn compress_string(s: String) -> String {
+    String::new()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    static TEST_CASES: [(&str, &str); 10] = [
+        ("aabcccccaaa", "a2b1c5a3"),
+        ("abcde", "abcde"),
+        ("yyyeewxxxddassss", "y3e2w1x3d2a1s4"),
+        ("xxgeaf", "xxgeaf"),
+        ("iiiixwwwwe", "i4x1w4e1"),
+        ("aabAAAbbCCCC", "a2b1A3b2C4"),
+        ("ggeeaa", "ggeeaa"),
+        ("jjjaabb", "j3a2b2"),
+        ("llldee", "llldee"),
+        ("bbbkkpooo", "b3k2p1o3")
+    ];
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_compress() {
+        for (input, expected_res) in TEST_CASES {
+            let res = compress_string(String::from(input));
+            assert_eq!(res, expected_res);
+        }
     }
 }
