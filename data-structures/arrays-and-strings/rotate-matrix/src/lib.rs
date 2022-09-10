bc18008f8264d30f41017fc81881c613c9ca696d
Basil Eagle <basileagle@gmail.com>
2022-09-10 11:44:55 +0100

    created test cases for rotate_matrix

--- data-structures/arrays-and-strings/rotate-matrix/src/lib.rs
+++ data-structures/arrays-and-strings/rotate-matrix/src/lib.rs
@@ -2,8 +2,17 @@
 // image is represented by an integer, write a method to rotate the image by 90
 // degrees. Can you do this in place?
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn rotate_matrix<const N: usize>(m: [[i32; N]; N]) -> [[i32; N]; N] {
+    // [
+    //     [0, 1],
+    //     [0, 1]
+    // ]
+    for i in m {
+        for j in i {
+            println!("{j}");
+        }
+    }
+    m
 }
 
 #[cfg(test)]
@@ -11,8 +20,45 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn rotation_test() {
+        let matrix_one = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]
+        ];
+        let res_one = rotate_matrix(matrix_one);
+        assert_eq!(res_one, [
+            [13, 9, 5, 1],
+            [14, 10, 6, 2],
+            [15, 11, 7, 3],
+            [16, 12, 8, 4]
+        ]);
+
+        let matrix_two = [
+            [0, 1],
+            [0, 1]
+        ];
+        let res_two = rotate_matrix(matrix_two);
+        assert_eq!(res_two, [
+            [0, 0],
+            [1, 1]
+        ]);
+
+        let matrix_three = [
+            [4, 4, 4, 3, 4],
+            [3, 3, 3, 4, 3],
+            [1, 2, 3, 4, 5],
+            [1, 1, 1, 2, 1],
+            [0, 0, 0, 1, 0]
+        ];
+        let res_three = rotate_matrix(matrix_three);
+        assert_eq!(res_three, [
+            [0, 1, 1, 3, 4],
+            [0, 1, 2, 3, 4],
+            [0, 1, 3, 3, 4],
+            [1, 2, 4, 4, 3],
+            [0, 1, 5, 3, 4]
+        ]);
     }
 }
