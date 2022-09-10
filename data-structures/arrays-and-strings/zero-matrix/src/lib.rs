15905da264273f4c07d20f9be8e4b0fd1d3b5042
Basil Eagle <basileagle@gmail.com>
2022-09-10 16:12:45 +0100

    init zero-matrix

---
+++ data-structures/arrays-and-strings/zero-matrix/src/lib.rs
@@ -0,0 +1,17 @@
+// Write an algorithm such that if an element in an M x N matrix is 0, its entire row
+// and column are set to 0.
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
