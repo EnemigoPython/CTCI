68dea13db82d4b3bc4bc134d5806f721c8e99bb4
Basil Eagle <basileagle@gmail.com>
2022-09-11 21:46:16 +0100

    completed string-rotation

--- data-structures/arrays-and-strings/string-rotation/src/lib.rs
+++ data-structures/arrays-and-strings/string-rotation/src/lib.rs
@@ -3,8 +3,30 @@
 // is a rotation of s1 using only one call to `isSubstring` (e.g. "waterbottle"
 // is a rotation of "erbottlewat").
 
+pub fn string_idx(s: &str, i: usize) -> char {
+    s.as_bytes()[i] as char
+}
+
+pub fn is_substring(s1: &str, s2: &str) -> bool {
+    s1.contains(&s2)
+}
+
 pub fn is_rotated_string(s1: String, s2: String) -> bool {
-    true
+    println!("{} {}", s1, s2);
+    if s1.len() != s2.len() || s1 == s2 { return false }
+    let first_char = string_idx(&s1, 0);
+    let mut inclusive_idx = 0;
+    for c in s2.chars() {
+        let compare_c = string_idx(&s1, inclusive_idx);
+        if c == compare_c {
+            inclusive_idx += 1;
+        } else {
+            inclusive_idx = if c == first_char { 1 } else { 0 };
+        }
+    }
+    if inclusive_idx == 0 { return false }
+    let str_range = 0..s2.len()-inclusive_idx;
+    is_substring(&s1, &s2[str_range])
 }
 
 #[cfg(test)]
