3ad84ad13e8826b6168ed31bcda9f62d123a6a41
Basil Eagle <basileagle@gmail.com>
2022-09-12 22:32:34 +0100

    implement new for linked list

--- data-structures/linked-lists/src/lib.rs
+++ data-structures/linked-lists/src/lib.rs
@@ -1,22 +1,22 @@
 /// We could write `use std::collections::LinkedList`...
 /// Or we could write it ourselves!
-pub struct LinkedList <'a, T> {
+pub struct LinkedList<'a, T> {
     head: Node<'a, T>,
 }
 
-impl <'a, T> LinkedList <'a, T> {
-    fn new(_value: T) -> Self {
+impl<'a, T: std::clone::Clone> LinkedList<'a, T> {
+    fn new(values: Vec<T>) -> Self {
         LinkedList {
             head: Node {
                 next: None,
-                value: _value,
-            }
+                value: values.last().cloned().unwrap(),
+            },
         }
     }
 }
 
 /// A single node in the linked list
-pub struct Node <'a, T> {
+pub struct Node<'a, T> {
     next: Option<&'a Node<'a, T>>,
     value: T,
 }
