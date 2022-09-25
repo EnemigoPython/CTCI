58a0d5a900458d6cbb0159fde2a38a7fad83141b
Basil Eagle <basileagle@gmail.com>
2022-09-25 10:35:33 +0100

    added iter and iter mut

--- data-structures/linked-lists/src/lib.rs
+++ data-structures/linked-lists/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::binary_heap::Iter;
+
 type Link<T> = Option<Box<Node<T>>>;
 
 struct Node<T> {
@@ -36,6 +38,70 @@ impl<T> List<T> {
         }
         list
     }
+
+    fn to_vec(self) -> Vec<T> {
+        let mut v: Vec<T> = Vec::new();
+        for i in self.into_iter() {
+            v.push(i);
+        }
+        v
+    }
+
+    fn into_iter(self) -> ListIntoIter<T> {
+        ListIntoIter(self)
+    }
+
+    fn iter(&self) -> ListIter<T> {
+        ListIter {
+            next: self.head.as_deref(),
+        }
+    }
+
+    fn iter_mut(&mut self) -> ListIterMut<T> {
+        ListIterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
+}
+
+struct ListIntoIter<T>(List<T>);
+
+impl<T> Iterator for ListIntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+struct ListIter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for ListIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.value
+        })
+    }
+}
+
+struct ListIterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T> Iterator for ListIterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.value
+        })
+    }
 }
 
 /// remove_dups prompt: Write code to remove duplicates from an unsorted linked list.
@@ -56,5 +122,22 @@ mod tests {
         assert_eq!(list.pop(), Some(3));
         assert_eq!(list.pop(), Some(2));
         assert_eq!(list.pop(), Some(1));
+    }
+
+    #[test]
+    fn test_iter() {
+        let list: List<i32> = List::from_vec(vec![4, 3, 2, 1, 0]);
+        for (e, i) in list.iter().enumerate() {
+            assert_eq!(e, *i as usize);
+        }
+    }
+
+    #[test]
+    fn test_iter_mut() {
+        let mut list: List<i32> = List::from_vec(vec![3, 2, 1]);
+        for val in list.iter_mut() {
+            *val += 1;
+        }
+        assert_eq!(list.to_vec(), vec![2, 3, 4]);
     }
 }
