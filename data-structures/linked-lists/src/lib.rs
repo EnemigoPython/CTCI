3585fcee6c792fd433e139623f92510fe4b94d51
Basil Eagle <basileagle@gmail.com>
2022-09-27 10:45:36 +0100

    prompt for partition

--- data-structures/linked-lists/src/lib.rs
+++ data-structures/linked-lists/src/lib.rs
@@ -149,6 +149,18 @@ pub mod linked_list {
                 curr = &mut node.next;
             }
         }
+
+        /// partition prompt: Write code to partition a linked list around a value x, such that all
+        /// nodes less than x come before all nodes greater than or equal to x. (IMPORTANT: The
+        /// partition element x can appear anywhere in the "right partition": it does not need to 
+        /// appear between the left and right partitions. The additional spacing in the example below
+        /// indicates the partition. Yes, the output below is one of many valid outputs!)
+        /// EXAMPLE
+        /// Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition=5]
+        /// Output: 3 -> 1 -> 2     ->      10 -> 5 -> 5 -> 8
+        pub fn partition(&mut self) {
+
+        }
     }
 
     pub struct ListIntoIter<T>(List<T>);
